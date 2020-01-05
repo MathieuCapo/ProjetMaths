249363350ce2798be3226470c689fc5474a898b9
Mathieu <mathieu.capo@iut-rodez.fr>
2020-01-05 12:34:00 -0500

    correction binomiale

--- src/application/outils/Binomiale.java
+++ src/application/outils/Binomiale.java
@@ -14,44 +14,45 @@ import java.util.Scanner;
  * du nombre de fois ou l'utilisateur � gagn� et on compare � l'esp�rance
  */
 public class Binomiale {
-	
+
 	/**	 Nombre d'exp�rience � r�aliser */
-	private int nb;
-	
+	private double nb;
+
 	/** probabilit� de succ�s de l'exp�rience */
 	private double p ;
-        
-        private final static int NB_EXPERIENCE = 1000;
-	
+
 	/** liste des r�sultats obtenue lors de l'exp�rience de Bernouilli */
 	private ArrayList<Integer> conteneurChemin;
+
+	private double nbExperience;
 	
 	/** ensemble des moyennes, � renvoyer pour avoir l'ensemble des r�sultats des tests */
 	private Double[] contientNbApparition;
-	
+
 	/** Moyenne de succ�s obtenue */
 	private Double moyenne;
-	
+
 	private Double nbApparition;
-        
+
 	/** scanner des entrees utilisateur */
 	private Scanner entree = new Scanner(System.in);
-	
-	
+
+
 	/** 
 	 * constructeur de la Loi
 	 */
-	public Binomiale(Double proba, int NbLance) {
+	public Binomiale(double proba, double NbLance,Double nbExperience) {
 		//entreeNbLance(); // initialise le nombre de lancer (nb)
 		//entreeValProb();  // initialise la valeur de succ�s (p)
 		this.p = proba;
 		this.nb = NbLance;
+		this.nbExperience = nbExperience;
 		moyenne = 0.0;
 		nbApparition = 0.0;
 		conteneurChemin = new ArrayList<Integer>();
-                contientNbApparition = new Double[NB_EXPERIENCE];
+		contientNbApparition = new Double[(int) this.nbExperience];
 	}	
-	
+
 	/**
 	 * Realisation de  l'experience de Bernoulli avec la proba de succ�s donn� par l'utilisateur
 	 * @param probasucces probabilit� de succ�s de tomber sur 1 ( vrai ) lors de l'exp�rience
@@ -71,40 +72,42 @@ public class Binomiale {
 	 * @return la moyenne du succ�s
 	 */
 	public Double moyenneApparition() {
-            for(int i = 0; i < conteneurChemin.size(); i++) {
-                    if(conteneurChemin.get(i).equals(1)) {
-                            moyenne += 1; 
-                    }
-            }
-            System.out.println(moyenne);
-            nbApparition = moyenne;
-            moyenne /= conteneurChemin.size();
-           return moyenne;
+		for(int i = 0; i < conteneurChemin.size(); i++) {
+			if(conteneurChemin.get(i).equals(1)) {
+				moyenne += 1; 
+			}
+		}
+		//System.out.println(moyenne);
+		nbApparition = moyenne;
+		moyenne /= conteneurChemin.size();
+		return moyenne;
 	}
-	
+
 	public Double[] getcontientNbApparition() {
 		return contientNbApparition;
 	}
-	
+
 	/**
-	 * A lancer depuis l'interface
+	 * Permet de lancer la simulation
 	 * @param args
+	 * @return 
 	 */
-	public void main(String args[] ) {
+	public Double[] simuler(Double nbExperience) {
 		/* Lancer le constructeur quand on ouvre la page interface */
 		/* ACTION LORSQU'ON CLIQUE SUR SIMULER */
-		for ( int y = 0; y < NB_EXPERIENCE; y++) {
+		for ( int y = 0; y < nbExperience; y++) {
 			for(int i = 0; i < nb; i++) {
 				conteneurChemin.add(Bernouilli());
 			}
 			moyenne = moyenneApparition();
 			System.out.println("moyenne d'apparition de la valeur correct lors de la simulation : " + moyenne
-								+ "\nproba de succ�s : " + p);
-                    contientNbApparition[y] = nbApparition;
-                    System.out.println(nbApparition);
-                    nbApparition = 0.0;
-                    moyenne = 0.0;
-                    conteneurChemin = new ArrayList<Integer>();
+					+ "\nproba de succ�s : " + p);
+			contientNbApparition[y] = nbApparition;
+			System.out.println("nombre d'apparition : " + nbApparition + '\n' + '\n');
+			nbApparition = 0.0;
+			moyenne = 0.0;
+			conteneurChemin = new ArrayList<Integer>();
 		}
+		return contientNbApparition;
 	}
 }
